26aac569888831fec868bdea336c81dcb3121ae2
Matthew Fournier <fournier.matthew@gmail.com>
2017-12-29 15:42:56 -0500

    Create default state

--- assets/js/reducers/BaseReducer.js
+++ assets/js/reducers/BaseReducer.js
@@ -1,8 +1,21 @@
 import { ACTIONS } from "../actions/Actions"
 
 import { socket, channel } from "../socket"
+static DEFAULT_STATE = {
+  meta: {},
+  checklist: {
+    items: [
+      {
+        id: 1,
+        checked: false,
+        current_owner: nil,
+        content: "Test Item"
+      }
+    ]
+  }
+}
 
-function BaseReducer(state={}, action) {
+function BaseReducer(state=DEFAULT_STATE, action) {
   return state
 }
 
