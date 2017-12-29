1dcbc90b42d71cb1ee6c30a5e7c8f5ca6bb09611
Matthew Fournier <fournier.matthew@gmail.com>
2017-12-29 09:14:44 -0500

    Add checklist page.
    Add route, controller function, template.

--- lib/methedras_web/controllers/page_controller.ex
+++ lib/methedras_web/controllers/page_controller.ex
@@ -4,4 +4,8 @@ defmodule MethedrasWeb.PageController do
   def index(conn, _params) do
     render conn, "index.html"
   end
+
+  def checklist(conn, _params) do
+    render conn, "checklist.html"
+  end
 end
--- lib/methedras_web/router.ex
+++ lib/methedras_web/router.ex
@@ -17,6 +17,7 @@ defmodule MethedrasWeb.Router do
     pipe_through :browser # Use the default browser stack
 
     get "/", PageController, :index
+    get "/checklist", PageController, :checklist
   end
 
   # Other scopes may use custom stacks.
