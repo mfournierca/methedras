be7b49a0d38d662bd728b5d4e0623031cc355d10
Matthew Fournier <fournier.matthew@gmail.com>
2018-01-03 13:10:46 -0500

    Use explicit jquery.ajax() method so we can set headers

--- assets/js/utils/api.js
+++ assets/js/utils/api.js
@@ -1,11 +1,17 @@
-const request = require("es6-request")
 const jquery = require("jquery")
 import { ACTIONS } from "../actions/Actions"
 
 const saveStateToServer = (dispatch, state) => {
   var dest = "http://" + window.location.host.toString() + "/checklist"
-  console.log("posting to " + dest)
-  jquery.post(dest, state)
+  console.log("posting to " + dest + ", csrf token: " + window.csrf_token)
+  jquery
+    .ajax({
+      type: "post",
+      url: dest,
+      data: state,
+      dataType: "json"
+      /* headers: { } */
+    })
     .fail(() => { dispatch({type: ACTIONS.LOG, message: "save state post fail"}) })
     .done(() => { dispatch({type: ACTIONS.LOG, message: "save state post done"}) })
     .always(() => { dispatch({type: ACTIONS.LOG, message: "save state post always"}) });
