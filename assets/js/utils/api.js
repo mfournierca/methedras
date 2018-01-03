4ff5b74c31b88d1a747ffa0246ec2184d949df33
Matthew Fournier <fournier.matthew@gmail.com>
2018-01-03 13:34:15 -0500

    Add CSRF token to jquery POST request

--- assets/js/utils/api.js
+++ assets/js/utils/api.js
@@ -3,14 +3,25 @@ import { ACTIONS } from "../actions/Actions"
 
 const saveStateToServer = (dispatch, state) => {
   var dest = "http://" + window.location.host.toString() + "/checklist"
-  console.log("posting to " + dest + ", csrf token: " + window.csrf_token)
+
+  var csrf = null
+  var meta_tags = document.getElementsByTagName("meta")
+  for (var i = 0; i < meta_tags.length; i++) {
+    if (meta_tags[i].getAttribute("name") == "csrf-token") {
+      csrf = meta_tags[i].getAttribute("content")
+      break
+    }
+  }
+
   jquery
     .ajax({
       type: "post",
       url: dest,
       data: state,
-      dataType: "json"
-      /* headers: { } */
+      dataType: "json",
+      headers: {
+        "X-CSRF-TOKEN": csrf
+      }
     })
     .fail(() => { dispatch({type: ACTIONS.LOG, message: "save state post fail"}) })
     .done(() => { dispatch({type: ACTIONS.LOG, message: "save state post done"}) })
