558a81dd4feba96e2a493d7c457011779080ba86
Matthew Fournier <fournier.matthew@gmail.com>
2018-02-17 16:01:40 -0500

    Rename reducers file

--- assets/js/store/Store.js
+++ assets/js/store/Store.js
@@ -2,7 +2,7 @@ import { createStore, applyMiddleware } from 'redux'
 import { routerMiddleware } from 'react-router-redux'
 import thunkMiddleware from 'redux-thunk'
 
-import reducers from "../reducers";
+import reducers from "../reducers/Reducer";
 
 export default function configureStore(browserHistory) {
   const reduxRouterMiddleWare = routerMiddleware(browserHistory);
