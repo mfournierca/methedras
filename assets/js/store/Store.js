5e7674e2e74a44459ee553ecd83873d6d7098376
Matthew Fournier <fournier.matthew@gmail.com>
2018-02-17 11:57:32 -0500

    Add router middleware to redux store

--- assets/js/store/Store.js
+++ assets/js/store/Store.js
@@ -1,8 +1,14 @@
 import { createStore, applyMiddleware } from 'redux'
+import { routerMiddleware } from 'react-router-redux'
 import thunkMiddleware from 'redux-thunk'
 
 import BaseReducer from "../reducers/BaseReducer"
 
-const createStoreWithMiddleware = applyMiddleware(thunkMiddleware)(createStore)
-const Store = createStoreWithMiddleware(BaseReducer)
-export default Store
+export default function configureStore(browserHistory) {
+  const reduxRouterMiddleWare = routerMiddleware(browserHistory);
+
+  const createStoreWithMiddleware = applyMiddleware(reduxRouterMiddleWare, thunkMiddleware)(createStore);
+  const Store = createStoreWithMiddleware(BaseReducer);
+
+  return Store;
+}
