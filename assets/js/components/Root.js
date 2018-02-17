92e7336675c5bc531b27a8f133762aee463ed737
Matthew Fournier <fournier.matthew@gmail.com>
2018-02-17 12:28:01 -0500

    Add Root component

---
+++ assets/js/components/Root.js
@@ -0,0 +1,20 @@
+import React                from 'react';
+import { Provider }         from 'react-redux';
+import { Router }           from 'react-router';
+import { invariant }        from 'invariant';
+import { RoutingContext }   from 'react-router';
+import configRoutes         from '../routes';
+
+export default class Root extends React.Component {
+  render() {
+    const { store } = this.props;
+
+    return (
+      <Provider store={ store }>
+        <Router history={ this.props.routerHistory }>
+          { configRoutes(store) }
+        </Router>
+      </Provider>
+    );
+  }
+}
