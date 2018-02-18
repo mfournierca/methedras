70126ce35212c48e203dc3f9c1d5a18e83fe0b69
Matthew Fournier <fournier.matthew@gmail.com>
2018-02-18 12:16:17 -0500

    Remove new, show, update ChecklistApp controller functions

--- lib/methedras_web/controllers/checklist_app_controller.ex
+++ lib/methedras_web/controllers/checklist_app_controller.ex
@@ -9,11 +9,6 @@ defmodule MethedrasWeb.ChecklistAppController do
     render(conn, "index.html", checklists: checklists)
   end
 
-  def new(conn, _params) do
-    changeset = Catalog.change_checklist_app(%ChecklistApp{})
-    render(conn, "new.html", changeset: changeset)
-  end
-
   def create(conn, %{}) do
     case Catalog.create_checklist(%{"data": %{"title": "default checklist title", "items": []}}) do
       {:ok, checklist} ->
@@ -27,26 +22,8 @@ defmodule MethedrasWeb.ChecklistAppController do
     end
   end
 
-  def show(conn, %{"id" => id}) do
-    checklist_app = Catalog.get_checklist_app!(id)
-    render(conn, "show.html", checklist_app: checklist_app)
-  end
-
   def checklist_edit(conn, %{"id" => _}) do
     render(conn, "edit.html")
-  end
-
-  def update(conn, %{"id" => id, "checklist_app" => checklist_app_params}) do
-    checklist_app = Catalog.get_checklist_app!(id)
-
-    case Catalog.update_checklist_app(checklist_app, checklist_app_params) do
-      {:ok, checklist_app} ->
-        conn
-        |> put_flash(:info, "Checklist app updated successfully.")
-        |> redirect(to: checklist_app_path(conn, :show, checklist_app))
-      {:error, %Ecto.Changeset{} = changeset} ->
-        render(conn, "edit.html", checklist_app: checklist_app, changeset: changeset)
-    end
   end
 
   def delete(conn, %{"id" => id}) do
